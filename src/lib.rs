c2145a25e66fec695f3434ca0edb7ae30c4375dc
Kai Hudalla <kai.hudalla@bosch.com>
2024-07-17 15:21:08 +0200

    Improve documentation in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -11,39 +11,47 @@
  * SPDX-License-Identifier: Apache-2.0
  ********************************************************************************/
 
-//! # up-rust  - uProtocol Rust library
-//!
-//! The purpose of this crate is to provide Rust specific code to work with the various
-//! [uProtocol Core API types](https://github.com/eclipse-uprotocol/up-spec/tree/main/up-core-api).
-//! The crate contains trait definitions and convenience functionality for building, converting,
-//! validating and serializing uProtocol data types.
-//!
-//! ## Library contents
-//!
-//! * `communication` module, which defines uProtocol's Communication Layer API for publishing and subscribing to topics and invoking RPC methods.
-//!   It also contains a default implementation employing the Transport Layer API.
-//! * `uattributes` module, with uProtocol message attribute types and validators
-//! * `umessage` module, which defines the uProtocol core message type and provides related convenience functionality
-//! * `upayload` module, which defines payload representation for uProtocol messages
-//! * `uri` module, providing convenience wrappers for creation and validation of uProtocol-style resource identifiers
-//! * `ustatus` module, which provices uProtocol types for representing status and status codes
-//! * `utransport` module, as an interface contract between uProtocol and specific transport protocol implementations
-//! * `uuid` module, which generates and validates UUIDs as per the uProtocol specification
-//!
-//! For user convenience, all of these modules export their types on up_rust top-level, except for (future) optional features.
-//!
-//! ## Features
-//!
-//! * `communication` contains the [Communication Layer API](https://github.com/eclipse-uprotocol/up-spec/blob/main/up-l2/api.adoc) and its
-//!   default implementation on top of the [Transport Layer API](https://github.com/eclipse-uprotocol/up-spec/blob/main/up-l1/README.adoc).
-//!   Enabled by default.
-//! * `udiscovery` contains the generated protobuf stubs for [uProtocol Core API uDiscovery](https://raw.githubusercontent.com/eclipse-uprotocol/up-spec/main/up-l3/udiscovery/v3/README.adoc).
-//!   Enabled by default.
-//! * `usubscription` contains the generated protobuf stubs for [uProtocol Core API uSubscription](https://raw.githubusercontent.com/eclipse-uprotocol/up-spec/main/up-l3/usubscription/v3/README.adoc).
-//! * `utwin` feature, which contains the generated protobuf stubs for [uProtocol Core API uTwin](https://raw.githubusercontent.com/eclipse-uprotocol/up-spec/main/up-l3/utwin/v3/README.adoc)
-//!
-//! ## References
-//! * [Eclipse-uProtocol Specification](https://github.com/eclipse-uprotocol/up-spec)
+/*!
+up-rust is the [Eclipse uProtocol&trade; Language Library](https://github.com/eclipse-uprotocol/uprotocol-spec/blob/main/languages.adoc) for the
+Rust programming language.
+
+This crate can be used to
+
+* implement uEntities that communicate with each other using the uProtocol [Communication Layer API](https://github.com/eclipse-uprotocol/up-spec/blob/main/up-l2/api.adoc)
+  over one of the supported transport protocols.
+* implement support for an additional transport protocol by means of implementing the [Transport Layer API](https://github.com/eclipse-uprotocol/up-spec/blob/main/up-l1/README.adoc).
+
+## Library contents
+
+* `communication` module, which defines uProtocol's Communication Layer API for publishing and subscribing to topics and invoking RPC methods.
+  It also contains a default implementation employing the Transport Layer API.
+* `uattributes` module, with uProtocol message attribute types and validators
+* `umessage` module, which defines the uProtocol core message type and provides related convenience functionality
+* `upayload` module, which defines payload representation for uProtocol messages
+* `uri` module, providing convenience wrappers for creation and validation of uProtocol-style resource identifiers
+* `ustatus` module, which provices uProtocol types for representing status and status codes
+* `utransport` module, as an interface contract between uProtocol and specific transport protocol implementations
+* `uuid` module, which generates and validates UUIDs as per the uProtocol specification
+
+For user convenience, all of these modules export their types on up_rust top-level, except for (future) optional features.
+
+## Features
+
+* `communication` enables support for the [Communication Layer API](https://github.com/eclipse-uprotocol/up-spec/blob/main/up-l2/api.adoc) and its
+  default implementation on top of the [Transport Layer API](https://github.com/eclipse-uprotocol/up-spec/blob/main/up-l1/README.adoc).
+  Enabled by default.
+* `udiscovery` enables support for types required to interact with [uDiscovery service](https://raw.githubusercontent.com/eclipse-uprotocol/up-spec/main/up-l3/udiscovery/v3/README.adoc)
+  implementations.
+* `usubscription` enables support for types required to interact with [uSubscription service](https://raw.githubusercontent.com/eclipse-uprotocol/up-spec/main/up-l3/usubscription/v3/README.adoc)
+  implementations. Enabled by default.
+* `utwin` enables support for types required to interact with [uTwin service](https://raw.githubusercontent.com/eclipse-uprotocol/up-spec/main/up-l3/utwin/v3/README.adoc)
+  implementations.
+
+## References
+
+* [uProtocol Specification](https://github.com/eclipse-uprotocol/up-spec)
+
+*/
 
 // up_core_api types used and augmented by up_rust - symbols re-exported to toplevel, errors are module-specific
 #[cfg(feature = "communication")]
