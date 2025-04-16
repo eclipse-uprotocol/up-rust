SoftwareDefinedVehicle/uprotocol-sdk-rust_fork

Use Gherkin to define TCK for UUID and UUri serialization
The up-tck component had been using Gherkin for defining the TCK's
test scenarios. The idea was that these could be used for verifying a
language library's compliance with the specification.

This is a first step in the direction described in
https://github.com/eclipse-uprotocol/up-tck/issues/116.

The TCK's test scenarios have been adapted to cover more cases and, in
particular, also cover failure cases. The corresponding unit tests have
been replaced by Cucumber based tests and the Gherkin files have been
amended with OpenFastTrace specification identifiers and are now also
included when doing the requirements tracing run.

features/uuid/protobuf_serialization.feature
@@ -1,0 +1,33 @@
+#
+# Copyright (c) 2025 Contributors to the Eclipse Foundation
+#
+# See the NOTICE file(s) distributed with this work for additional
+# information regarding copyright ownership.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Apache License Version 2.0 which is available at
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# SPDX-FileType: SOURCE
+# SPDX-License-Identifier: Apache-2.0
+#
+Feature: Efficient binary encoding of uProtocol UUIDs
+
+  Scenario Outline:
+    Developers using a uProtocol language library should be able to get the binary
+    encoding of a uProtocol UUID instance as specified by the UUID proto3 definition file.
+
+    [utest->req~uuid-proto~1]
+
+    The byte sequences representing the Protocol Buffer encodings have been created
+    using https://www.protobufpal.com/ based on the UUID proto3 definition from the
+    uProtocol specification.
+
+    Given a UUID having MSB <uuid_msb> and LSB <uuid_lsb>
+    When serializing the UUID to its protobuf wire format
+    Then the resulting byte sequence is <protobuf>
+    And the original UUID can be recreated from the protobuf wire format
+
+    Examples:
+      | uuid_msb           | uuid_lsb           | protobuf                             |
+      | 0x0000000000017000 | 0x8010101010101a1a | 090070010000000000111a1a101010101080 |

features/uuri/protobuf_serialization.feature
@@ -1,0 +1,46 @@
+#
+# Copyright (c) 2025 Contributors to the Eclipse Foundation
+#
+# See the NOTICE file(s) distributed with this work for additional
+# information regarding copyright ownership.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Apache License Version 2.0 which is available at
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# SPDX-FileType: SOURCE
+# SPDX-License-Identifier: Apache-2.0
+#
+Feature: Efficient binary encoding of endpoint identifiers (UUri)
+
+  Scenario Outline:
+    Developers using a uProtocol language library should be able to get the binary
+    encoding of a UUri instance as specified by the UUri proto3 definition file.
+
+    // [utest->req~uri-data-model-proto~1]
+
+    The byte sequences representing the Protocol Buffer encodings have been created
+    using https://www.protobufpal.com/ based on the UUri proto3 definition from the
+    uProtocol specification.
+
+    Given a UUri having authority <authority_name>
+    And having entity identifier <entity_id>
+    And having major version <version>
+    And having resource identifier <resource_id>
+    When serializing the UUri to its protobuf wire format
+    Then the resulting byte sequence is <protobuf>
+    And the original UUri can be recreated from the protobuf wire format
+
+    Examples:
+      | authority_name | entity_id  | version | resource_id | protobuf                                         |
+      | ""             | 0x00000001 |    0x01 |      0xa1fb |                                 1001180120fbc302 |
+      | "my_vin"       | 0x10000001 |    0x02 |      0x001a |             0a066d795f76696e1081808080011802201a |
+      | "*"            | 0x00000101 |    0xa0 |      0xa1fb |                       0a012a10810218a00120fbc302 |
+      | "mcu1"         | 0x0000FFFF |    0x01 |      0xa1fb |                 0a046d63753110ffff03180120fbc302 |
+      | "vcu.my_vin"   | 0x01a40101 |    0x01 |      0x8000 |   0a0a7663752e6d795f76696e108182900d180120808002 |
+      | "vcu.my_vin"   | 0xFFFF0101 |    0x01 |      0xa1fb | 0a0a7663752e6d795f76696e108182fcff0f180120fbc302 |
+      | "vcu.my_vin"   | 0xFFFFFFFF |    0x01 |      0xa1fb | 0a0a7663752e6d795f76696e10ffffffff0f180120fbc302 |
+      | "vcu.my_vin"   | 0x00000101 |    0x00 |      0xa1fb |           0a0a7663752e6d795f76696e10810220fbc302 |
+      | "vcu.my_vin"   | 0x00000101 |    0xFF |      0xa1fb |     0a0a7663752e6d795f76696e10810218ff0120fbc302 |
+      | "vcu.my_vin"   | 0x00000101 |    0x01 |      0x0000 |               0a0a7663752e6d795f76696e1081021801 |
+      | "vcu.my_vin"   | 0x00000101 |    0x01 |      0xFFFF |       0a0a7663752e6d795f76696e108102180120ffff03 |

tests/tck_uuri.rs
@@ -1,0 +1,177 @@
+/********************************************************************************
+ * Copyright (c) 2025 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ********************************************************************************/
+
+use std::str::FromStr;
+
+use common::CustomTckOpts;
+use cucumber::{cli, given, then, when, writer, World};
+use protobuf::Message;
+use up_rust::UUri;
+
+mod common;
+
+fn value_as_u8(value: String) -> u8 {
+    if value.starts_with("0x") || value.starts_with("0X") {
+        u8::from_str_radix(trimhex(&value), 16).expect("not a hex number")
+    } else {
+        value.parse().expect("not an integer number")
+    }
+}
+
+fn value_as_u16(value: String) -> u16 {
+    if value.starts_with("0x") || value.starts_with("0X") {
+        u16::from_str_radix(trimhex(&value), 16).expect("not a hex number")
+    } else {
+        value.parse().expect("not an integer number")
+    }
+}
+
+fn value_as_u32(value: String) -> u32 {
+    if value.starts_with("0x") || value.starts_with("0X") {
+        u32::from_str_radix(trimhex(&value), 16).expect("not a hex number")
+    } else {
+        value.parse().expect("not an integer number")
+    }
+}
+
+fn trimhex(s: &str) -> &str {
+    s.strip_prefix("0x")
+        .unwrap_or(s.strip_prefix("0X").unwrap_or(s))
+}
+
+#[derive(cucumber::World, Default, Debug)]
+struct UUriWorld {
+    uuri: UUri,
+    uri: String,
+    protobuf: Vec<u8>,
+    error: Option<Box<dyn std::error::Error>>,
+}
+
+#[given(expr = "a URI string {word}")]
+async fn with_uri_string(w: &mut UUriWorld, uri_string: String) {
+    w.uri = uri_string;
+}
+
+#[given(expr = "a UUri having authority {string}")]
+async fn with_authority(w: &mut UUriWorld, authority_name: String) {
+    w.uuri.authority_name = authority_name;
+}
+
+#[given(expr = "having entity identifier {word}")]
+async fn with_entity_id(w: &mut UUriWorld, entity_id: String) {
+    w.uuri.ue_id = value_as_u32(entity_id);
+}
+
+#[given(expr = "having major version {word}")]
+async fn with_major_version(w: &mut UUriWorld, major_version: String) {
+    w.uuri.ue_version_major = value_as_u32(major_version);
+}
+
+#[given(expr = "having resource identifier {word}")]
+async fn with_resource_id(w: &mut UUriWorld, resource_id: String) {
+    w.uuri.resource_id = value_as_u32(resource_id);
+}
+
+#[when(expr = "serializing the UUri to a URI")]
+async fn serialize_to_uri(w: &mut UUriWorld) {
+    w.uri = w.uuri.to_uri(true);
+}
+
+#[when(expr = "serializing the UUri to its protobuf wire format")]
+async fn serialize_to_protobuf(w: &mut UUriWorld) {
+    w.protobuf = w
+        .uuri
+        .write_to_bytes()
+        .expect("failed to serialize UUri to protobuf");
+}
+
+#[when(expr = "deserializing the URI to a UUri")]
+async fn deserialize_from_uri(w: &mut UUriWorld) {
+    match UUri::from_str(w.uri.as_str()) {
+        Ok(uuri) => {
+            w.uuri = uuri;
+        }
+        Err(e) => {
+            w.error = Some(Box::from(e));
+        }
+    }
+}
+
+#[then(expr = "the resulting byte sequence is {word}")]
+async fn assert_protobuf(w: &mut UUriWorld, hex_string: String) {
+    let buf = hex::decode(trimhex(&hex_string)).expect("not a valid hex string");
+    assert_eq!(w.protobuf, buf);
+}
+
+#[then(expr = "the resulting URI string is {word}")]
+async fn assert_uri_string(w: &mut UUriWorld, expected_uri: String) {
+    assert_eq!(w.uri, expected_uri);
+}
+
+#[then(expr = "the UUri has authority {string}")]
+async fn assert_authority(w: &mut UUriWorld, value: String) {
+    assert_eq!(w.uuri.authority_name(), value);
+}
+
+#[then(expr = "has entity identifier {word}")]
+async fn assert_entity_id(w: &mut UUriWorld, entity_id: String) {
+    assert_eq!(w.uuri.ue_id, value_as_u32(entity_id));
+}
+
+#[then(expr = "has major version {word}")]
+async fn assert_major_version(w: &mut UUriWorld, major_version: String) {
+    assert_eq!(w.uuri.uentity_major_version(), value_as_u8(major_version));
+}
+
+#[then(expr = "has resource identifier {word}")]
+async fn assert_resource_id(w: &mut UUriWorld, resource_id: String) {
+    assert_eq!(w.uuri.resource_id(), value_as_u16(resource_id));
+}
+
+#[then(expr = "the attempt fails")]
+async fn assert_failure(w: &mut UUriWorld) {
+    assert!(w.error.is_some());
+}
+
+#[then(expr = "the original UUri can be recreated from the protobuf wire format")]
+async fn assert_original_uuri_can_be_recreated_from_protobuf(w: &mut UUriWorld) {
+    assert!(UUri::parse_from_bytes(&w.protobuf).is_ok_and(|uuri| w.uuri.eq(&uuri)));
+}
+
+#[then(expr = "the original UUri can be recreated from the URI string")]
+async fn assert_original_uuri_can_be_recreated_from_uri_string(w: &mut UUriWorld) {
+    assert!(w.uri.parse::<UUri>().is_ok_and(|uuri| w.uuri.eq(&uuri)));
+}
+
+#[then(expr = "the UUri matches pattern {word}")]
+async fn assert_uuri_matches_pattern(w: &mut UUriWorld, pattern: String) {
+    assert!(pattern.parse::<UUri>().is_ok_and(|p| p.matches(&w.uuri)));
+}
+
+#[then(expr = "the UUri does not match pattern {word}")]
+async fn assert_uuri_does_not_match_pattern(w: &mut UUriWorld, pattern: String) {
+    assert!(pattern.parse::<UUri>().is_ok_and(|p| !p.matches(&w.uuri)));
+}
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let opts = cli::Opts::<_, _, _, CustomTckOpts>::parsed();
+    let out_file = opts.custom.get_junit_report_file("uuri")?;
+
+    UUriWorld::cucumber()
+        .with_cli(opts)
+        .with_writer(writer::JUnit::new(out_file, 0))
+        .run("features/uuri")
+        .await;
+    Ok(())
+}

eclipse-uprotocol/up-rust

features/uuid/protobuf_serialization.feature
@@ -1,0 +1,33 @@
+#
+# Copyright (c) 2025 Contributors to the Eclipse Foundation
+#
+# See the NOTICE file(s) distributed with this work for additional
+# information regarding copyright ownership.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Apache License Version 2.0 which is available at
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# SPDX-FileType: SOURCE
+# SPDX-License-Identifier: Apache-2.0
+#
+Feature: Efficient binary encoding of uProtocol UUIDs
+
+  Scenario Outline:
+    Developers using a uProtocol language library should be able to get the binary
+    encoding of a uProtocol UUID instance as specified by the UUID proto3 definition file.
+
+    [utest->req~uuid-proto~1]
+
+    The byte sequences representing the Protocol Buffer encodings have been created
+    using https://www.protobufpal.com/ based on the UUID proto3 definition from the
+    uProtocol specification.
+
+    Given a UUID having MSB <uuid_msb> and LSB <uuid_lsb>
+    When serializing the UUID to its protobuf wire format
+    Then the original UUID can be recreated from the protobuf wire format
+    And the same UUID can be deserialized from <byte_sequence>
+
+    Examples:
+      | uuid_msb           | uuid_lsb           | byte_sequence                        |
+      | 0x0000000000017000 | 0x8010101010101a1a | 090070010000000000111a1a101010101080 |

features/uuri/protobuf_serialization.feature
@@ -1,0 +1,46 @@
+#
+# Copyright (c) 2025 Contributors to the Eclipse Foundation
+#
+# See the NOTICE file(s) distributed with this work for additional
+# information regarding copyright ownership.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Apache License Version 2.0 which is available at
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# SPDX-FileType: SOURCE
+# SPDX-License-Identifier: Apache-2.0
+#
+Feature: Efficient binary encoding of endpoint identifiers (UUri)
+
+  Scenario Outline:
+    Developers using a uProtocol language library should be able to get the binary
+    encoding of a UUri instance as specified by the UUri proto3 definition file.
+
+    // [utest->req~uri-data-model-proto~1]
+
+    The byte sequences representing the Protocol Buffer encodings have been created
+    using https://www.protobufpal.com/ based on the UUri proto3 definition from the
+    uProtocol specification.
+
+    Given a UUri having authority <authority_name>
+    And having entity identifier <entity_id>
+    And having major version <version>
+    And having resource identifier <resource_id>
+    When serializing the UUri to its protobuf wire format
+    Then the original UUri can be recreated from the protobuf wire format
+    And the same UUri can be deserialized from <byte_sequence>
+
+    Examples:
+      | authority_name | entity_id  | version | resource_id | byte_sequence                                    |
+      | ""             | 0x00000001 |    0x01 |      0xa1fb |                                 1001180120fbc302 |
+      | "my_vin"       | 0x10000001 |    0x02 |      0x001a |             0a066d795f76696e1081808080011802201a |
+      | "*"            | 0x00000101 |    0xa0 |      0xa1fb |                       0a012a10810218a00120fbc302 |
+      | "mcu1"         | 0x0000FFFF |    0x01 |      0xa1fb |                 0a046d63753110ffff03180120fbc302 |
+      | "vcu.my_vin"   | 0x01a40101 |    0x01 |      0x8000 |   0a0a7663752e6d795f76696e108182900d180120808002 |
+      | "vcu.my_vin"   | 0xFFFF0101 |    0x01 |      0xa1fb | 0a0a7663752e6d795f76696e108182fcff0f180120fbc302 |
+      | "vcu.my_vin"   | 0xFFFFFFFF |    0x01 |      0xa1fb | 0a0a7663752e6d795f76696e10ffffffff0f180120fbc302 |
+      | "vcu.my_vin"   | 0x00000101 |    0x00 |      0xa1fb |           0a0a7663752e6d795f76696e10810220fbc302 |
+      | "vcu.my_vin"   | 0x00000101 |    0xFF |      0xa1fb |     0a0a7663752e6d795f76696e10810218ff0120fbc302 |
+      | "vcu.my_vin"   | 0x00000101 |    0x01 |      0x0000 |               0a0a7663752e6d795f76696e1081021801 |
+      | "vcu.my_vin"   | 0x00000101 |    0x01 |      0xFFFF |       0a0a7663752e6d795f76696e108102180120ffff03 |

tests/tck_uuri.rs
@@ -1,0 +1,185 @@
+/********************************************************************************
+ * Copyright (c) 2025 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ********************************************************************************/
+
+use std::str::FromStr;
+
+use common::CustomTckOpts;
+use cucumber::{cli, given, then, when, writer, World};
+use protobuf::Message;
+use up_rust::UUri;
+
+mod common;
+
+const FEATURES_PATH: &str = "features/uuri";
+
+fn value_as_u8(value: String) -> u8 {
+    if value.starts_with("0x") || value.starts_with("0X") {
+        u8::from_str_radix(trimhex(&value), 16).expect("not a hex number")
+    } else {
+        value.parse().expect("not an integer number")
+    }
+}
+
+fn value_as_u16(value: String) -> u16 {
+    if value.starts_with("0x") || value.starts_with("0X") {
+        u16::from_str_radix(trimhex(&value), 16).expect("not a hex number")
+    } else {
+        value.parse().expect("not an integer number")
+    }
+}
+
+fn value_as_u32(value: String) -> u32 {
+    if value.starts_with("0x") || value.starts_with("0X") {
+        u32::from_str_radix(trimhex(&value), 16).expect("not a hex number")
+    } else {
+        value.parse().expect("not an integer number")
+    }
+}
+
+fn trimhex(s: &str) -> &str {
+    s.strip_prefix("0x")
+        .unwrap_or(s.strip_prefix("0X").unwrap_or(s))
+}
+
+#[derive(cucumber::World, Default, Debug)]
+struct UUriWorld {
+    uuri: UUri,
+    uri: String,
+    protobuf: Vec<u8>,
+    error: Option<Box<dyn std::error::Error>>,
+}
+
+#[given(expr = "a URI string {word}")]
+async fn with_uri_string(w: &mut UUriWorld, uri_string: String) {
+    w.uri = uri_string;
+}
+
+#[given(expr = "a UUri having authority {string}")]
+async fn with_authority(w: &mut UUriWorld, authority_name: String) {
+    w.uuri.authority_name = authority_name;
+}
+
+#[given(expr = "having entity identifier {word}")]
+async fn with_entity_id(w: &mut UUriWorld, entity_id: String) {
+    w.uuri.ue_id = value_as_u32(entity_id);
+}
+
+#[given(expr = "having major version {word}")]
+async fn with_major_version(w: &mut UUriWorld, major_version: String) {
+    w.uuri.ue_version_major = value_as_u32(major_version);
+}
+
+#[given(expr = "having resource identifier {word}")]
+async fn with_resource_id(w: &mut UUriWorld, resource_id: String) {
+    w.uuri.resource_id = value_as_u32(resource_id);
+}
+
+#[when(expr = "serializing the UUri to a URI")]
+async fn serialize_to_uri(w: &mut UUriWorld) {
+    w.uri = w.uuri.to_uri(true);
+}
+
+#[when(expr = "serializing the UUri to its protobuf wire format")]
+async fn serialize_to_protobuf(w: &mut UUriWorld) {
+    w.protobuf = w
+        .uuri
+        .write_to_bytes()
+        .expect("failed to serialize UUri to protobuf");
+}
+
+#[when(expr = "deserializing the URI to a UUri")]
+async fn deserialize_from_uri(w: &mut UUriWorld) {
+    match UUri::from_str(w.uri.as_str()) {
+        Ok(uuri) => {
+            w.uuri = uuri;
+        }
+        Err(e) => {
+            w.error = Some(Box::from(e));
+        }
+    }
+}
+
+#[then(expr = "the resulting URI string is {word}")]
+async fn assert_uri_string(w: &mut UUriWorld, expected_uri: String) {
+    assert_eq!(w.uri, expected_uri);
+}
+
+#[then(expr = "the UUri has authority {string}")]
+async fn assert_authority(w: &mut UUriWorld, value: String) {
+    assert_eq!(w.uuri.authority_name(), value);
+}
+
+#[then(expr = "has entity identifier {word}")]
+async fn assert_entity_id(w: &mut UUriWorld, entity_id: String) {
+    assert_eq!(w.uuri.ue_id, value_as_u32(entity_id));
+}
+
+#[then(expr = "has major version {word}")]
+async fn assert_major_version(w: &mut UUriWorld, major_version: String) {
+    assert_eq!(w.uuri.uentity_major_version(), value_as_u8(major_version));
+}
+
+#[then(expr = "has resource identifier {word}")]
+async fn assert_resource_id(w: &mut UUriWorld, resource_id: String) {
+    assert_eq!(w.uuri.resource_id(), value_as_u16(resource_id));
+}
+
+#[then(expr = "the attempt fails")]
+async fn assert_failure(w: &mut UUriWorld) {
+    assert!(w.error.is_some());
+}
+
+#[then(expr = "the original UUri can be recreated from the protobuf wire format")]
+async fn assert_original_uuri_can_be_recreated_from_protobuf(w: &mut UUriWorld) {
+    assert!(UUri::parse_from_bytes(&w.protobuf).is_ok_and(|uuri| w.uuri.eq(&uuri)));
+}
+
+#[then(expr = "the same UUri can be deserialized from {word}")]
+async fn assert_uuri_can_be_deserialized_from_bytes(w: &mut UUriWorld, hex_string: String) {
+    let buf = hex::decode(trimhex(&hex_string)).expect("not a valid hex string");
+    assert!(UUri::parse_from_bytes(buf.as_slice()).is_ok_and(|uuri| w.uuri.eq(&uuri)));
+}
+
+#[then(expr = "the original UUri can be recreated from the URI string")]
+async fn assert_original_uuri_can_be_recreated_from_uri_string(w: &mut UUriWorld) {
+    assert!(w.uri.parse::<UUri>().is_ok_and(|uuri| w.uuri.eq(&uuri)));
+}
+
+#[then(expr = "the UUri matches pattern {word}")]
+async fn assert_uuri_matches_pattern(w: &mut UUriWorld, pattern: String) {
+    assert!(pattern.parse::<UUri>().is_ok_and(|p| p.matches(&w.uuri)));
+}
+
+#[then(expr = "the UUri does not match pattern {word}")]
+async fn assert_uuri_does_not_match_pattern(w: &mut UUriWorld, pattern: String) {
+    assert!(pattern.parse::<UUri>().is_ok_and(|p| !p.matches(&w.uuri)));
+}
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let opts = cli::Opts::<_, _, _, CustomTckOpts>::parsed();
+    if let Some(file) = opts.custom.get_junit_out_file("uuri") {
+        UUriWorld::cucumber()
+            .with_cli(opts)
+            .with_writer(cucumber::writer::JUnit::new(file, 0))
+            .run(FEATURES_PATH)
+            .await;
+    } else {
+        UUriWorld::cucumber()
+            .with_cli(opts)
+            .with_writer(writer::Basic::stdout())
+            .run(FEATURES_PATH)
+            .await;
+    }
+    Ok(())
+}