f43ad77418ea87c91d1f53804c199743d70ad908
Kenneth Mead <kennethmead784@gmail.com>
2025-08-01 12:43:39 +0000

    wrapped mac workaround script in OS check

--- .devcontainer/scripts/functions/install-rust-toolchain.sh
+++ .devcontainer/scripts/functions/install-rust-toolchain.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 install_rust_toolchain() {
     source ${WORKSPACE_FOLDER}/.devcontainer/scripts/functions/get-arch-and-os.sh
-    read -r TARGET_ARCH TARGET_OS <<< $(get_arch_and_os)
+    read -r TARGET_ARCH TARGET_OS <<< "$(get_arch_and_os)"
     ARCH_AND_OS="${TARGET_ARCH}-${TARGET_OS}"
     echo "Detected architecture and OS: $ARCH_AND_OS"
     RUST_TOOLCHAIN="stable-$ARCH_AND_OS"
--- .devcontainer/scripts/post-create.sh
+++ .devcontainer/scripts/post-create.sh
@@ -2,18 +2,27 @@
 
 #region sudo stuff
 
-# This is a workaround which is againt necessary on MacOS 14.0, it looks like this bug is back:
-# https://github.com/microsoft/vscode-dev-containers/issues/1487#issuecomment-1143907307
-# grant permissions to mounted rust volume
-sudo chown vscode:vscode /rust-volume
+HOST_OS=""
+ARCH_AND_OS_FUNC="${WORKSPACE_FOLDER}.devcontainer/scripts/functions/get-arch-and-os.sh"
+if [[ -f "$ARCH_AND_OS_FUNC" ]]; then
+    source $ARCH_AND_OS_FUNC
+    read -r _ HOST_OS <<< "$(get_arch_and_os)"
+fi
+if [[ "$HOST_OS" == "darwin" ]]; then # darwin == Mac OS
+    # This is a workaround which is againt necessary on MacOS 14.0, it looks like this bug is back:
+    # https://github.com/microsoft/vscode-dev-containers/issues/1487#issuecomment-1143907307
+    # grant permissions to mounted rust volume
+    echo "(Mac OS only) Granting permissions to mounted rust volume"
+    sudo chown vscode:vscode /rust-volume
 
-# create /.cargo/config.toml in root folder
-sudo mkdir /.cargo/
-sudo touch /.cargo/config.toml
-sudo bash -c "cat << EOF > /.cargo/config.toml
-[build]
-target-dir = \"/rust-volume/target\"
-EOF"
+    # create /.cargo/config.toml in root folder
+    sudo mkdir /.cargo/
+    sudo touch /.cargo/config.toml
+    sudo bash -c "cat << EOF > /.cargo/config.toml
+    [build]
+    target-dir = \"/rust-volume/target\"
+    EOF"
+fi
 
 #endregion
 
